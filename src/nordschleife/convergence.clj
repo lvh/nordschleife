37eaa2c5ff1e0a96951277d669f58a31b1d9f72e
Laurens Van Houtven <_@lvh.io>
2015-03-05 14:30:45 -0800

    Make all of the tests pass

--- src/nordschleife/convergence.clj
+++ src/nordschleife/convergence.clj
@@ -1,4 +1,5 @@
 (ns nordschleife.convergence
+  (:require [com.palletops.jclouds.compute2 :refer :all])
   (:import [org.jclouds.compute.domain.internal NodeMetadataImpl]
            [org.jclouds.compute.domain NodeMetadata]))
 
@@ -6,7 +7,28 @@
   [status]
   `(. org.jclouds.compute.domain.NodeMetadata$Status ~status))
 
+(defn ^:private abs
+  [n]
+  (max n (- n)))
+
+(defn ^:private live
+  "Gets the live servers in a state."
+  [state]
+  (filter (some-fn running? pending?) (:servers state)))
+
 (defn measure-progress
   "Measures progress between states towards the desired state."
   [prev-state curr-state desired-state]
-  )
+  (let [prev-cap (count (live prev-state))
+        curr-cap (count (live curr-state))
+        delta (- curr-cap prev-cap)
+        dsrd-cap (:capacity desired-state)]
+    (cond
+      (< prev-cap dsrd-cap curr-cap) {:progress? false
+                                      :reason "overshoot"}
+      (< curr-cap dsrd-cap prev-cap) {:progress? false
+                                      :reason "undershoot"}
+      (or (and (pos? delta) (> curr-cap dsrd-cap))
+          (and (neg? delta) (< curr-cap dsrd-cap))
+          (zero? delta)) {:progress? false}
+      :default {:progress? true :amount (abs delta)})))
