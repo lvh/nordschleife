88d6361115af3b4c422003a128aaae57c62f48fe
Laurens Van Houtven <_@lvh.io>
2015-03-05 15:47:45 -0800

    Upgrade clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/test.check "0.7.0"]
                  [com.gfredericks/test.chuck "0.1.16"]
                  [aleph "0.4.0-beta3"]
