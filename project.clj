b91ed1ae6a7be8ada124a3c771621706e6556457
Laurens Van Houtven <_@lvh.io>
2015-03-05 12:05:22 -0800

    A bunch of deps

--- project.clj
+++ project.clj
@@ -5,7 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.7.0"]
-                 [com.gfredericks/test.chuck "0.1.16"]]
+                 [com.gfredericks/test.chuck "0.1.16"]
+                 [aleph "0.4.0-beta3"]
+                 [com.palletops/pallet "0.8.0-RC.11"]
+                 [com.palletops/pallet-jclouds "1.7.3"]
+                 [com.palletops/clj-jclouds "0.1.1"]
+                 [org.apache.jclouds/jclouds-allcompute "1.8.1"]
+                 [environ "1.0.0"]]
   :main ^:skip-aot nordschleife.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
