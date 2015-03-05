68c570a078b00aeb0fa4626f489039824dd099a2
Laurens Van Houtven <_@lvh.io>
2015-03-05 09:29:34 -0800

    Basic generators

---
+++ src/nordschleife/scenarios.clj
@@ -0,0 +1,30 @@
+(ns nordschleife.scenarios
+  (:require [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [medley.core :refer [map-vals]]))
+
+(defn weighted-consts
+  [weights-and-consts]
+  (gen/frequency
+   (for [[freq const] (seq weights-and-consts)]
+     [freq (gen/return const)])))
+
+(def scale-down
+  "A generator for scale down events.")
+
+(def server-failure
+  "A generator for server failures."
+  (gen/tuple (gen/return :server-failures)
+             (weighted-consts [[10 1] [2 2] [1 3]])))
+
+(def event-gen
+  "A generator for scenario events."
+  (gen/frequency [[10 (gen/return :acquiesce)]
+
+                  [6 (gen/return :scale-up)]
+                  [1 (gen/return :scale-up-%)]
+
+                  [6 (gen/return :scale-down)]
+                  [1 (gen/return :scale-down-%)]
+
+                  [6 server-failure-gen]]))
