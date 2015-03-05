9124565c7f0447f86790ab6b27bd61877f6ec9b2
Laurens Van Houtven <_@lvh.io>
2015-03-05 12:17:03 -0800

    Marginally more realistic test servers

--- test/nordschleife/convergence_test.clj
+++ test/nordschleife/convergence_test.clj
@@ -1,14 +1,13 @@
 (ns nordschleife.convergence-test
   (:require [nordschleife.convergence :refer :all]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all])
+  (:import [org.jclouds.compute.domain.internal NodeMetadataImpl]))
 
 (defn ^:private create-servers
-  "Creates some dummy servers for testing.
-
-  TODO: These should look like jClouds' NodeMetadata."
+  "Creates some dummy servers for testing."
   [n]
   (for [i (range n)]
-    {:id (str i)}))
+    (NodeMetadataImpl. nil (str i) (str i) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil)))
 
 (deftest measure-progress-tests
   (testing "If the capacity moves closer to the desired, progress has been made."
