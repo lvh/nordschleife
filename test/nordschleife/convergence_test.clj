e98b644cbeb0cd58d0ac75f9cb76f4c09b325ccf
Laurens Van Houtven <_@lvh.io>
2015-03-05 14:28:59 -0800

    ITYM concat

--- test/nordschleife/convergence_test.clj
+++ test/nordschleife/convergence_test.clj
@@ -65,21 +65,21 @@
              {:progress? false :reason "undershoot"}))))
   (testing "When some servers go from being pending to being errored, no progress was made."
     (let [working-servers (create-servers 2)
-          prev-state {:servers (conj working-servers (create-servers 2 PENDING))}
-          curr-state {:servers (conj working-servers (create-servers 2 ERROR))}
+          prev-state {:servers (concat working-servers (create-servers 2 PENDING))}
+          curr-state {:servers (concat working-servers (create-servers 2 ERROR))}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "When some servers go from build to error, no progress was made. That works correctly even if some of the errored machines get reaped in the mean while."
     (let [prev-state {:servers (create-servers 3 PENDING)}
-          curr-state {:servers (conj (create-servers 1 RUNNING)
+          curr-state {:servers (concat (create-servers 1 RUNNING)
                                      (create-servers 1 ERROR))}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "Errored servers are removed; no progress is made."
-    (let [prev-state {:servers (conj (create-servers 1 RUNNING)
-                                     (create-servers 3 ERROR))}
+    (let [prev-state {:servers (concat (create-servers 1 RUNNING)
+                                       (create-servers 3 ERROR))}
           curr-state {:servers (create-servers 1 RUNNING)}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
