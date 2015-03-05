92d7f1b5abb2e936c412d230241b3fbf1d21a08a
Laurens Van Houtven <_@lvh.io>
2015-03-05 13:18:55 -0800

    prep for being able to use statuses

--- test/nordschleife/convergence_test.clj
+++ test/nordschleife/convergence_test.clj
@@ -3,11 +3,32 @@
             [clojure.test :refer :all])
   (:import [org.jclouds.compute.domain.internal NodeMetadataImpl]))
 
+(def PENDING
+  (status PENDING))
+
+(def RUNNING
+  (status RUNNING))
+
+(def ERROR
+  (status ERROR))
+
 (defn ^:private create-servers
   "Creates some dummy servers for testing."
-  [n]
+  [n status]
   (for [i (range n)]
-    (NodeMetadataImpl. nil (str i) (str i) nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil)))
+    :let [provider-id "nordschleife"
+          name (str i)
+          id (str i)
+          location nil
+          uri nil
+          user-metadata nil
+          tags nil
+          group nil
+          hardware nil
+          image-id nil
+          os nil
+          backend-status nil]
+    (NodeMetadataImpl. provider-id name id location uri user-metadata tags group hardware image-id os status backend-status nil nil nil nil nil)))
 
 (deftest measure-progress-tests
   (testing "If the capacity moves closer to the desired, progress has been made."
