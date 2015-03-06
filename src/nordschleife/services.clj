7739458c7f0b43bfc8260326c6a39d4f9539cb01
Laurens Van Houtven <_@lvh.io>
2015-03-05 22:58:04 -0800

    Services includes an Auto Scale API

--- src/nordschleife/services.clj
+++ src/nordschleife/services.clj
@@ -1,9 +1,11 @@
 (ns nordschleife.services
   (:require [environ.core :refer [env]]
-            [com.palletops.jclouds.compute2 :refer :all]))
+            [com.palletops.jclouds.compute2 :refer :all]
+            [nordschleife.auto-scale :refer [auto-scale-api]]))
 
-(def compute
+(def services
   (let [compute-provider (env :nordschleife-provider)
         creds (map env [:nordschleife-username
                         :nordschleife-api-key])]
-    (apply compute-service (conj creds compute-provider))))
+    {:compute (apply compute-service (conj creds compute-provider))
+     :auto-scale (apply auto-scale-api creds)}))
