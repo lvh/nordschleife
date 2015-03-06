0f5a9b0eb56b53ff9b995cf17b3a5f5c5571602f
Laurens Van Houtven <_@lvh.io>
2015-03-06 10:23:09 -0800

    More tests

--- test/nordschleife/auto_scale_test.clj
+++ test/nordschleife/auto_scale_test.clj
@@ -13,4 +13,18 @@
                  (metadata {"xyzzy" "iddqd"})
                  (build))
           out (group-config in)]
-      (is (identical? in out)))))
+      (is (identical? in out))))
+  (testing "Maps get converted to GroupConfigurations"
+    (let [from-map (group-config {:name "my group"
+                                  :cooldown 10
+                                  :min-entities 0
+                                  :max-entities 10
+                                  :metadata {"xyzzy" "iddqd"}})
+          expected (.. (GroupConfiguration/builder)
+                       (name "my group")
+                       (cooldown 10)
+                       (minEntities 0)
+                       (maxEntities 10)
+                       (metadata {"xyzzy" "iddqd"})
+                       (build))]
+      (is (= from-map expected)))))
