7d84528086b583668a2cb669b57a34cb3d018a2a
Laurens Van Houtven <_@lvh.io>
2015-03-10 14:08:53 -0700

    gather-servers doesn't actually simplify anything

--- src/nordschleife/gathering.clj
+++ src/nordschleife/gathering.clj
@@ -1,14 +1,8 @@
 (ns nordschleife.gathering
-  "Gather information about the state of the cloud.")
-
-
-
-(defn ^:private gather-servers
-  "Gather all available servers."
-  [compute]
-  (nodes compute))
+  "Gather information about the state of the cloud."
+  (:require [com.palletops.jclouds.compute2 :refer [nodes]]))
 
 (defn gather
   "Gather all of the available information."
-  [compute]
-  {:servers (gather-servers compute)})
+  [services]
+  {:servers (nodes (:compute services))})
