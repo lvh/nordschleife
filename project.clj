255183f302b309be0e9a73fcaaf377fda9803751
Laurens Van Houtven <_@lvh.io>
2015-03-12 12:07:11 -0700

    Explain why some dependencies exist

--- project.clj
+++ project.clj
@@ -4,16 +4,28 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+
+                 ;; Generative testing
                  [org.clojure/test.check "0.7.0"]
                  [com.gfredericks/test.chuck "0.1.16"]
+
+                 ;; Web server/client
                  [aleph "0.4.0-beta3"]
+
+                 ;; Cloud interaction
                  [com.palletops/pallet "0.8.0-RC.11"]
                  [com.palletops/pallet-jclouds "1.7.3"]
                  [com.palletops/clj-jclouds "0.1.1"]
                  [org.apache.jclouds/jclouds-allcompute "1.7.3"]
                  [org.apache.jclouds.labs/rackspace-autoscale-us "1.7.3"]
+
+                 ;; Configuration
                  [environ "1.0.0"]
+
+                 ;; Asynchronous interactions
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+
+                 ;; Logging
                  [com.taoensso/timbre "3.4.0"]]
   :plugins [[lein-cljfmt "0.1.10"]]
   :main ^:skip-aot nordschleife.core
