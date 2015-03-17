c1f043f7308cc1c61c42b6c47515a7ee26e491fe
Laurens Van Houtven <_@lvh.io>
2015-03-17 16:22:40 -0700

    Write some tests

--- test/nordschleife/affect_test.clj
+++ test/nordschleife/affect_test.clj
@@ -1,20 +1,38 @@
 (ns nordschleife.affect-test
   (:require [nordschleife.affect :refer :all :as a]
+            [nordschleife.auto-scale :as as]
             [clojure.test :refer :all]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.properties :as prop]
             [nordschleife.scenarios :refer [scenario-gen]]))
 
 (def some-setup
-  {})
+  {:name "test group"})
+
+(def scale-up-event
+  {:type :scale-up :amount 5})
+
+(def scale-up-policy
+  {:cooldown 0
+   :name "scale-up by 5 policy for test group"
+   :type as/WEBHOOK
+   :target-type as/INCREMENTAL
+   :target "5"})
 
 (deftest required-policies-tests
   (are [evs expected] (= (@#'a/required-policies [some-setup evs])
                          expected)
-    [] #{}
-    [{:type :acquiesce}] #{}
-    [{:type :scale-up :amount 5}] #{{:type :scale-up :amount 5}}
-    (repeat 3 {:type :scale-up :amount 5}) #{{:type :scale-up :amount 5}}))
+       []
+       #{}
+
+       [{:type :acquiesce}]
+       #{}
+
+       [scale-up-event]
+       #{scale-up-policy}
+
+       (repeat 3 scale-up-event)
+       #{scale-up-policy}))
 
 (defspec required-policies-spec
   1000
@@ -22,5 +40,13 @@
    [scenario scenario-gen]
    (let [[_ evs] scenario
          policies (@#'a/required-policies scenario)]
-     (and (every? (comp #{:scale-up :scale-down :scale-to} :type) policies)
-          (every? (into #{} evs) policies)))))
+     (and
+      (set? policies)
+      (every? #(and (= (:cooldown %) 0)
+                    (= (:type %) as/WEBHOOK))
+              policies)
+      (every? (comp #{as/INCREMENTAL
+                      as/PERCENT_CHANGE
+                      as/DESIRED_CAPACITY}
+                    :target-type)
+              policies)))))
