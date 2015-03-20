3c8c46d6c615584ccc6b09640c93c100a281ac5f
Laurens Van Houtven <_@lvh.io>
2015-03-20 14:21:01 -0700

    Set up core for experimentation

--- src/nordschleife/core.clj
+++ src/nordschleife/core.clj
@@ -1,5 +1,16 @@
 (ns nordschleife.core
+  (:require [nordschleife.services :refer [services]]
+            [nordschleife.affect :refer [perform-scenarios]]
+            [nordschleife.scenarios :refer [sample]]
+            [nordschleife.gathering :refer [gather]])
   (:gen-class))
+
+(def the-scenario
+  (sample 1))
+
+(defn run-1
+  []
+  (perform-scenarios services (sample 10) 10))
 
 (defn -main
   "I don't do a whole lot ... yet."
