2675cfaf0ae1bb16ca9a0cb5a0a3b7f2deac62c2
Laurens Van Houtven <_@lvh.io>
2015-04-02 13:32:44 -0700

    Add a bunch of documentation

--- src/nordschleife/scenarios.clj
+++ src/nordschleife/scenarios.clj
@@ -5,10 +5,15 @@
             [com.gfredericks.test.chuck.generators :as gen']
             [clojure.math.numeric-tower :as math]))
 
-(def service-net "11111111-1111-1111-1111-111111111111")
-(def public-net "00000000-0000-0000-0000-000000000000")
+(def service-net
+  "The Rackspace identifier for the service network."
+  "11111111-1111-1111-1111-111111111111")
+(def public-net
+  "The Rackspace identifier for the public internet."
+  "00000000-0000-0000-0000-000000000000")
 
 (def launch-config-gen
+  "A generator for launch configurations."
   (gen/elements
    [{:load-balancers []
      :networks [service-net]
@@ -20,6 +25,12 @@
      :server-metadata {}}]))
 
 (def group-config-gen
+  "A generator for group configs."
+  ;; no-shrink tells test.check not to try and minimize this when it
+  ;; finds a failing test case. We're assuming that the name of the
+  ;; scaling group does not contribute to the failure, so it's
+  ;; pointless to find the shortest string that still causes the
+  ;; failure.
   (gen'/for [random-str (gen/no-shrink (gen'/string-from-regex #"[a-zA-Z0-9]{12}"))
              limits (gen/elements [{:cooldown 0
                                     :min-entities 0
@@ -29,12 +40,15 @@
         (assoc :name (str "nordschleife test group " random-str)))))
 
 (def setup-gen
+  "A generator for group setups."
   (gen'/for [group-config group-config-gen
              launch-config launch-config-gen]
     {:group-config group-config
      :launch-config launch-config}))
 
 (defn ^:private weighted-consts
+  "Given a set of [weight, const] tuples, makes a generator that will
+  randomly return one of the consts, weighted by respective weight."
   [weights-and-consts]
   (gen/frequency
    (for [[freq const] (seq weights-and-consts)]
@@ -67,10 +81,14 @@
                        weighted-events)))
 
 (defn add-tail-acquiesce
+  "At the end of a scenario, we should *always* acquiesce to see if
+  convergence eventually does the right thing."
   [evs]
   (conj evs {:type :acquiesce}))
 
 (defn coalesce-acquiesces
+  "If multiple :acquiesce events occur next to each other, they should
+  merge into one, since that is nonsensical by definition."
   [evs]
   (reduce (fn [evs ev]
             (if (= (:type ev) (:type (last evs)) :acquiesce)
@@ -79,7 +97,7 @@
           evs))
 
 (defn chop-head-acquiesce
-  "Acquiescing at the start is meaningless, so don't do it"
+  "Acquiescing at the start is meaningless, so don't do it."
   [evs]
   (if (= (:type (first evs)) :acquiesce)
     (rest evs)
@@ -94,10 +112,12 @@
   (gen/fmap clean-events (gen/vector event-gen 4 10)))
 
 (defn ^:private clamp
+  "Clamps a number to the given [low, high] interval (inclusive)."
   [x low high]
   (-> x (max low) (min high)))
 
 (defn ^:private compute-desired-states
+  "Given a scenario, find the desired state at the end of each step."
   [[setup evs]]
   (let [capacity (fn [event] (or (-> event :desired-state :capacity) 0))
         [min-cap max-cap] ((juxt :min-entities :max-entities)
@@ -122,6 +142,7 @@
   (gen/fmap compute-desired-states (gen/tuple setup-gen events-gen)))
 
 (def sample
+  "Sample the set of random scenarios."
   (partial gen/sample scenario-gen))
 
 (defn ^:private round-away-from-zero
