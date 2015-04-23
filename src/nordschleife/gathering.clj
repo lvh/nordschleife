c15b3f6e5df0c178407684fe20a21d8ba83d1ae2
Laurens Van Houtven <_@lvh.io>
2015-04-23 14:34:47 -0700

    Add a date to gathered events
    Aids debugging

--- src/nordschleife/gathering.clj
+++ src/nordschleife/gathering.clj
@@ -1,8 +1,10 @@
 (ns nordschleife.gathering
   "Gather information about the state of the cloud."
-  (:require [com.palletops.jclouds.compute2 :refer [nodes]]))
+  (:require [com.palletops.jclouds.compute2 :refer [nodes]])
+  (:import [java.util Date]))
 
 (defn gather
   "Gather all of the available information."
   [services]
-  {:servers (nodes (:compute services))})
+  {:timestamp (Date.)
+   :servers (nodes (:compute services))})
