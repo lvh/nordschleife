7b702d778d885575d69b58481aaa03066f73aea5
Laurens Van Houtven <_@lvh.io>
2015-04-23 20:03:44 -0700

    Cleanup

--- test/nordschleife/convergence_test.clj
+++ test/nordschleife/convergence_test.clj
@@ -18,40 +18,42 @@
 
 (defn ^:private create-servers
   "Creates some dummy servers for testing."
-  [n & {:keys [status base-name]
-        :or {status RUNNING
-             base-name "nordschleife"}}]
-  (for [i (range n)
-        :let [name (str base-name "-" i)
-              provider-id base-name
-              name name
-              id (str i)
-              location nil
-              uri nil
-              user-metadata {}
-              tags #{}
-              group nil
-              hardware nil
-              image-id nil
-              os nil
-              backend-status nil
-              login-port 1234
-              public-addrs ["1.2.3.4"]
-              private-addrs ["10.11.12.13"]
-              credentials nil
-              hostname name]]
-    (NodeMetadataImpl. provider-id name id location uri user-metadata tags
-                       group hardware image-id os status backend-status
-                       login-port public-addrs private-addrs credentials
-                       hostname)))
+  ([n]
+   (create-servers n {}))
+  ([n {:keys [status base-name]
+       :or {status RUNNING
+            base-name "nordschleife"}}]
+   (for [i (range n)
+         :let [name (str base-name "-" i)
+               provider-id base-name
+               name name
+               id (str i)
+               location nil
+               uri nil
+               user-metadata {}
+               tags #{}
+               group nil
+               hardware nil
+               image-id nil
+               os nil
+               backend-status nil
+               login-port 1234
+               public-addrs ["1.2.3.4"]
+               private-addrs ["10.11.12.13"]
+               credentials nil
+               hostname name]]
+     (NodeMetadataImpl. provider-id name id location uri user-metadata tags
+                        group hardware image-id os status backend-status
+                        login-port public-addrs private-addrs credentials
+                        hostname))))
 
 (deftest filter-group-tests
   (let [create-some (fn [group-name]
                       (let [base-name (str "server for " group-name)
                             create-some (fn [status]
                                           (create-servers 5
-                                                          :base-name base-name
-                                                          :status status))]
+                                                          {:base-name base-name
+                                                           :status status}))]
                         (mapcat create-some [RUNNING PENDING ERROR])))
         [xyzzy-servers iddqd-servers] (map create-some ["xyzzy" "iddqd"])
         all-servers (concat xyzzy-servers iddqd-servers)]
@@ -94,28 +96,23 @@
   (testing "servers going pending => error"
     (let [working-servers (create-servers 2)
           prev-state {:servers (concat working-servers
-                                       (create-servers 2 :status PENDING))}
+                                       (create-servers 2 {:status PENDING}))}
           curr-state {:servers (concat working-servers
-                                       (create-servers 2 :status ERROR))}
+                                       (create-servers 2 {:status ERROR}))}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "servers going pending -> error, with some being reaped"
-    (let [prev-state {:servers (create-servers 3 :status PENDING)}
-          curr-state {:servers (concat (create-servers 1 :status RUNNING)
-                                       (create-servers 1 :status ERROR))}
+    (let [prev-state {:servers (create-servers 3 {:status PENDING})}
+          curr-state {:servers (concat (create-servers 1 {:status RUNNING})
+                                       (create-servers 1 {:status ERROR}))}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "servers in error are reaped => no progress"
-    (let [prev-state {:servers (concat (create-servers 1 :status RUNNING)
-                                       (create-servers 3 :status ERROR))}
-          curr-state {:servers (create-servers 1 :status RUNNING)}
+    (let [prev-state {:servers (concat (create-servers 1 {:status RUNNING})
+                                       (create-servers 3 {:status ERROR}))}
+          curr-state {:servers (create-servers 1 {:status RUNNING})}
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false})))))
-
-(defn merge-states
-  [& states]
-  (into (empty (first states))
-        (map #(mapcat % states) (keys (first states)))))
