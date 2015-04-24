925b6667f1ae54a29299056cee48c255e7dbf730
Laurens Van Houtven <_@lvh.io>
2015-04-23 21:44:07 -0700

    Refactor

--- test/nordschleife/convergence_test.clj
+++ test/nordschleife/convergence_test.clj
@@ -1,6 +1,9 @@
 (ns nordschleife.convergence-test
   (:require [nordschleife.convergence :refer :all]
-            [clojure.test :refer :all])
+            [clojure.set :as set]
+            [clojure.test :refer :all]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop])
   (:import [org.jclouds.compute.domain.internal NodeMetadataImpl]))
 
 (defmacro status-const
@@ -47,6 +50,16 @@
                         login-port public-addrs private-addrs credentials
                         hostname))))
 
+(defn build-state
+  ([n]
+   (build-state n nil))
+  ([n server-args]
+   {:servers (create-servers n server-args)}))
+
+(defn merge-states
+  [& ss]
+  (apply merge-with concat ss))
+
 (deftest filter-group-tests
   (let [create (fn [group-name]
                  (let [base-name (str "server for " group-name)]
@@ -62,57 +75,63 @@
     (is (= (filter-group "iddqd" {:servers all-servers})
            {:servers iddqd-servers}))))
 
+(deftest pick-victims-tests
+  (let [group-name "xyzzy"
+        servers (create-servers 5 {:base-name group-name})
+        to-kill (pick-victims group-name {:servers servers} 3)]
+    (is (set/subset? (set to-kill) (set servers)))))
+
 (deftest measure-progress-tests
   (testing "reaching convergence"
-    (let [prev-state {:servers (create-servers 1)}
-          curr-state {:servers (create-servers 2)}
+    (let [prev-state (build-state 2)
+          curr-state (build-state 2)
           desired-state {:capacity 2}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? true :done? true}))))
   (testing "progress while scaling up"
-    (let [prev-state {:servers (create-servers 2)}
-          curr-state {:servers (create-servers 3)}
+    (let [prev-state (build-state 2)
+          curr-state (build-state 3)
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? true :amount 1}))))
   (testing "progress while scaling down"
-    (let [prev-state {:servers (create-servers 3)}
-          curr-state {:servers (create-servers 2)}
+    (let [prev-state (build-state 3)
+          curr-state (build-state 2)
           desired-state {:capacity 1}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? true :amount 1}))))
   (testing "overshoot (group was below desired, and is now above it)"
-    (let [prev-state {:servers (create-servers 4)}
-          curr-state {:servers (create-servers 6)}
+    (let [prev-state (build-state 4)
+          curr-state (build-state 6)
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false :reason "overshoot"}))))
   (testing "undershoot (group was above desired, and is now below it)"
-    (let [prev-state {:servers (create-servers 6)}
-          curr-state {:servers (create-servers 4)}
+    (let [prev-state (build-state 6)
+          curr-state (build-state 4)
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false :reason "undershoot"}))))
   (testing "servers going pending => error"
-    (let [working-servers (create-servers 2)
-          prev-state {:servers (concat working-servers
-                                       (create-servers 2 {:status PENDING}))}
-          curr-state {:servers (concat working-servers
-                                       (create-servers 2 {:status ERROR}))}
+    (let [working-state (build-state 2 {:status RUNNING})
+          prev-state (merge-states working-state
+                                   (build-state 2 {:status PENDING}))
+          curr-state (merge-states working-state
+                                   (build-state 2 {:status ERROR}))
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "servers going pending -> error, with some being reaped"
-    (let [prev-state {:servers (create-servers 3 {:status PENDING})}
-          curr-state {:servers (concat (create-servers 1 {:status RUNNING})
-                                       (create-servers 1 {:status ERROR}))}
+    (let [prev-state (build-state 3 {:status PENDING})
+          curr-state (merge-states (build-state 1 {:status RUNNING})
+                                   (build-state 1 {:status ERROR}))
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false}))))
   (testing "servers in error are reaped => no progress"
-    (let [prev-state {:servers (concat (create-servers 1 {:status RUNNING})
-                                       (create-servers 3 {:status ERROR}))}
-          curr-state {:servers (create-servers 1 {:status RUNNING})}
+    (let [prev-state (merge-states (build-state 1 {:status RUNNING})
+                                   (build-state 3 {:status ERROR}))
+          curr-state (build-state 1 {:status RUNNING})
           desired-state {:capacity 5}]
       (is (= (measure-progress prev-state curr-state desired-state)
              {:progress? false})))))
